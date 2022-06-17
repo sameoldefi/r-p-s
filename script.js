bded35e73e9b1755d83e6534af14ac69c24f0ab3
sameoldefi <105250912+sameoldefi@users.noreply.github.com>
2022-06-17 19:18:50 +0300

    Upgrade to the html page and added new features in js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// let userChoice;
 let win = 0
 let lose = 0
 let tie = 0
 let wrongAnswer = 0
 
-
 function computerPlay(){
     const choice = ['rock', 'paper', 'scissors'];
     let computerRandom = Math.floor(Math.random()* 3) ;
     let computerSelection = choice[computerRandom];
-    // console.log(computerSelection);
     return computerSelection;
 }
 
@@ -20,61 +17,83 @@ let computerChoice = computerPlay();
 
 if ( userChoice.toLowerCase() === 'rock' && computerChoice == 'rock' ){
     tie++
-return tieResult ="Player: " + userChoice + '\n' +  "Computer: "+ computerChoice + '\n' + "It's A Tie ! ! ! ";
+    div.textContent= computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    gameResult.textContent = "you lose this time" + lose + ' ' + win;
+    return gameResult.textContent + ' ' + gameResult.textContent;
 }
 else if (userChoice.toLowerCase() === 'rock' && computerChoice == 'paper'){
     lose++;
-    return loseResult = "Player: " + userChoice + '\n' + "Computer: "+ computerChoice + '\n' + "You Lost";
+    div.textContent= computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    gameResult.textContent = "you lose this time" + lose + ' ' + win;
+    return gameResult.textContent + ' ' + gameResult.textContent;
 }
 else if (userChoice.toLowerCase() === 'rock' && computerChoice == 'scissors'){
     win++
-    return  winResult = "Player: " + userChoice + '\n' + "Computer: "+ computerChoice + '\n' + "You Won!!!";
+    div.textContent= computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    gameResult.textContent = "you lose this time" + lose + ' ' + win;
+    return gameResult.textContent + ' ' + gameResult.textContent;
 }
 
 // ----------------------  Player choice - Paper ----------------------
 
 else if ( userChoice.toLowerCase() === 'paper' && computerChoice == 'rock' ){
     win++
-    return  winResult = "Player: " + userChoice + '\n' + "Computer: "+ computerChoice + '\n' +"You Won!!! Paper eats Rock";
+    div.textContent= computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    gameResult.textContent = "you lose this time" + lose + ' ' + win;
+    return gameResult.textContent + ' ' + gameResult.textContent;    
 }
 else if(userChoice.toLowerCase() === 'paper' && computerChoice == 'paper'){
     tie++
-    return tieResult = "Player: " + userChoice + '\n' + "Computer: "+ computerChoice + '\n' +"It's A Tie ! ! ! Paper and Paper are friends";
+    div.textContent= computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    gameResult.textContent = "you lose this time" + lose + ' ' + win;
+    return gameResult.textContent + ' ' + gameResult.textContent;
+
 }
 else if(userChoice.toLowerCase() === 'paper' && computerChoice == 'scissors'){
     lose++
-    return loseResult =  "Player: " + userChoice + '\n' + "Computer: "+ computerChoice + '\n' +"You Lost Paper loses to scissors";
+    div.textContent= computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    gameResult.textContent = "you lose this time" + lose + ' ' + win;
+    return gameResult.textContent + ' ' + gameResult.textContent;
 }
 
 // ---------------------- Player choice - scissors ----------------------
 
 else if (userChoice.toLowerCase() === 'scissors' && computerChoice == 'rock'){
     lose++
-    return loseResult = "Player: " + userChoice + '\n' + "Computer: "+ computerChoice + '\n' +"You Lost";
+     div.textContent = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+     gameResult.textContent = "you lose this time" + lose + ' ' + win;
+    return gameResult.textContent + ' ' + gameResult.textContent;
+
 }
 else if (userChoice.toLowerCase() === 'scissors' && computerChoice == 'paper'){
     win++
-    return  winResult = "Player: " + userChoice + '\n' + "Computer: "+ computerChoice + '\n' +"You Won!!!";
+    div.textContent= computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    gameResult.textContent = "you lose this time" + lose + ' ' + win;
+    return gameResult.textContent + ' ' + gameResult.textContent;
 }
 else if (userChoice.toLowerCase() === 'scissors' && computerChoice == 'scissors'){
     tie++
-    return tieResult = "Player: " + userChoice + '\n' + "Computer: "+ computerChoice + '\n' +"It's A Tie ! ! ! ";
-}
-else {
-    wrongAnswer++
-    return notRight = "You need to write rock, paper or scissors";
+    div.textContent= computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    gameResult.textContent = "you lose this time" + lose + ' ' + win;
+    return gameResult.textContent + ' ' + gameResult.textContent;
 }
 
+if (win >= 1) {
+    console.log("hello");
+}
 }
 
 // ---------------------- Start Game ----------------------
 
-const rockBtn = document.querySelector('#button1');
-const paperBtn = document.querySelector('#button2');
-const scissorsBtn = document.querySelector('#button3');
 
-rockBtn.addEventListener('click', () => console.log(result('Rock')));
-paperBtn.addEventListener('click', () => console.log(result('Paper')));
-scissorsBtn.addEventListener('click', () => console.log(result('Scissors')));
+const div = document.querySelector('#computersResult');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const gameResult = document.querySelector('#gameResult');
+
+rockBtn.addEventListener('click', () => result('Rock'));
+paperBtn.addEventListener('click', () => result('Paper'));
+scissorsBtn.addEventListener('click', () => result('Scissors'));
 
  
